f87b402215c645c15d562875cd682ef77cb97866
Jadhiel Vélez <jadhielv@gmail.com>
2019-10-22 22:57:48 -0400

    defining new approach

--- App/Program.cs
+++ App/Program.cs
@@ -10,10 +10,19 @@ namespace App
         {
             Console.WriteLine("Hello World!");
             var schema = Schema.For(@"
-                type Query {
-                    hello: String
+                type Jedi {
+                    name: String,
+                    side: String
                 }
-            ");
+
+                type Query {
+                    hello: String,
+                    jedis: [Jedi]
+                }
+                ", _ =>
+                {
+                    _.Types.Include<Query>();
+                });
 
             var root = new { Hello = "Hello World!" };
             var json = schema.Execute(_ =>
@@ -24,5 +33,26 @@ namespace App
 
             Console.WriteLine(json);
         }
+    }
+
+    public class Query
+    {
+        [GraphQLMetadata("jedis")]
+        public IEnumerable<Jedi> GetJedis()
+        {
+            return StarWarsDB.GetJedis();
+        }
+
+        [GraphQLMetadata("hello")]
+        public string GetHello()
+        {
+            return "Hello Query class";
+        }
+
+        public static IEnumerable<Jedi> GetJedis() => new List<Jedi>() {
+                new Jedi() { Name ="Luke", Side="Light"},
+                new Jedi() { Name ="Yoda", Side="Light"},
+                new Jedi() { Name ="Darth Vader", Side="Dark"}
+             };
     }
 }
