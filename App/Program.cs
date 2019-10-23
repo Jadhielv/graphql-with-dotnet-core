aff3ded609c262a6cf1dfabe32ed022f7a265c58
Jadhiel Vélez <jadhielv@gmail.com>
2019-10-22 22:18:22 -0400

    first commit

---
+++ App/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using GraphQL;
+using GraphQL.Types;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var schema = Schema.For(@"
+                type Query {
+                    hello: String
+                }
+            ");
+
+            var root = new { Hello = "Hello World!" };
+            var json = schema.Execute(_ =>
+            {
+                _.Query = "{ hello }";
+                _.Root = root;
+            });
+
+            Console.WriteLine(json);
+        }
+    }
+}
